651125148617fa4ca1324a3512fee54ffba930df
Soham Maity <sohammaity239@gmail.com>
2025-11-25 23:46:30 +0530

    feat: update API_BASE to production URL

--- docs/config.js
+++ docs/config.js
@@ -1,2 +1,2 @@
-window.API_BASE = "http://localhost:8888"; // Backend URL
+window.API_BASE = "https://cooked-roasted-life.onrender.com"; // Backend URL
 window.FRONTEND_BASE = window.location.origin;
